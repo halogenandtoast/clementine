def032a7f723b61106248d3419c3c1db455d81cf
halogenandtoast <halogenandtoast@gmail.com>
2014-07-01 10:54:41 -0400

    Fix gem building

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,12 +1,36 @@
 require 'fileutils'
 
 EXT_DIR = File.dirname(__FILE__)
+ROOT_DIR = File.join(EXT_DIR, "..")
 CLOJURESCRIPT_HOME = File.join(File.dirname(__FILE__), "clojure-clojurescript")
+
+def is_git_repo?
+  File.exists?(File.join(ROOT_DIR, ".git"))
+end
 
 $stdout.puts "Bootrapping ClojureScript"
 
 Dir.chdir(EXT_DIR) do
-  unless File.exists?("clojure-clojurescript")
+  File.open("./Makefile", "w") do |file|
+    file.puts <<-MAKEFILE
+all:
+\t@echo "Done"
+install:
+\t@echo
+    MAKEFILE
+  end
+
+  if File.exists?("clojure-clojurescript")
+    FileUtils.rm_rf "./clojure-clojurescript"
+  end
+
+  if is_git_repo?
+    Dir.chdir(ROOT_DIR) do
+      %x(git submodule add https://github.com/clojure/clojurescript.git ext/clojure-clojurescript)
+      %x(git submodule init)
+      %x(git submodule update)
+    end
+  else
     %x(git clone https://github.com/clojure/clojurescript.git clojure-clojurescript)
   end
 end
@@ -15,3 +39,5 @@ Dir.chdir(CLOJURESCRIPT_HOME) do
   %x(./script/bootstrap)
   FileUtils.rm_rf File.join('./closure')
 end
+
+
